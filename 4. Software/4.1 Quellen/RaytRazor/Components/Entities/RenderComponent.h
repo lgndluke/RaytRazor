0848fa056d6774e5c98df635f8071b7a0dbd642e
lgndluke <github@lgndluke.com>
2024-11-04 17:21:34 +0100

    RenderComponent.cpp + RenderComponent.h: Neu: RenderComponent Klasse hinzugefügt.

---
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Entities/RenderComponent.h
@@ -0,0 +1,25 @@
+#ifndef RENDERCOMPONENT_H
+#define RENDERCOMPONENT_H
+
+//TODO Add imports here.
+
+using namespace std;
+
+/**
+ * @brief //TODO
+ * @author //TODO
+ */
+class RenderComponent
+{
+
+    public:
+
+        //TODO add public Method definitions.
+
+    private:
+
+        //TODO add private Method definitions and attributes.
+
+};
+
+#endif
