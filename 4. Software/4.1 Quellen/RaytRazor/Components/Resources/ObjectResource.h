016e551b37759885d8ea9e51ab7e31b8f85c2c91
lgndluke <github@lgndluke.com>
2024-11-04 17:21:16 +0100

    ObjectResource.cpp + ObjectResource.h: Neu: ObjectResource Klasse hinzugefügt.

---
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Resources/ObjectResource.h
@@ -0,0 +1,25 @@
+#ifndef OBJECTRESOURCE_H
+#define OBJECTRESOURCE_H
+
+//TODO Add imports here.
+
+using namespace std;
+
+/**
+ * @brief //TODO
+ * @author //TODO
+ */
+class ObjectResource
+{
+
+    public:
+
+        //TODO add public Method definitions.
+
+    private:
+
+        //TODO add private Method definitions and attributes.
+
+};
+
+#endif
