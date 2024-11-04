d5614b73a2167a5e2b55150e50221b5eb24343f8
lgndluke <github@lgndluke.com>
2024-11-04 17:21:00 +0100

    MaterialResource.cpp + MaterialResource.h: Neu: MaterialResource Klasse hinzugefügt.

---
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Resources/MaterialResource.h
@@ -0,0 +1,25 @@
+#ifndef MATERIALRESOURCE_H
+#define MATERIALRESOURCE_H
+
+//TODO Add imports here.
+
+using namespace std;
+
+/**
+ * @brief //TODO
+ * @author //TODO
+ */
+class MaterialResource
+{
+
+    public:
+
+        //TODO add public Method definitions.
+
+    private:
+
+        //TODO add private Method definitions and attributes.
+
+};
+
+#endif
