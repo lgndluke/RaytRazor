922913625064765cd520a2f00f2db8cc61fe0c6b
lgndluke <github@lgndluke.com>
2024-11-12 20:02:34 +0100

    Parsing Verzeichnis: Struktur/Klassen des "Parsing" Verzeichnisses angepasst/hinzugefügt.

---
+++ 4. Software/4.1 Quellen/RaytRazor/Parsing/Json_Parser.cpp
@@ -0,0 +1,33 @@
+#include "Json_Parser.h"
+#include <../../../Utility/External_Libraries/nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+
+
+//TODO
+// - Define further tasks here.
+// - task2
+// - task3
+
+using namespace std;
+using json = nlohmann::json;
+
+// Attributes (Init of global/static Attributes)
+
+// Constructor
+Json_Parser::Json_Parser() = default;
+
+// Destructor
+
+// Private Methods
+
+// Public Methods
+void parseJSON(const string& pathToJson)
+{
+
+}
+
+void exportToJSON(const string& exportPath)
+{
+
+}
---
+++ 4. Software/4.1 Quellen/RaytRazor/Parsing/Json_Parser.h
@@ -0,0 +1,52 @@
+#ifndef JSON_PARSER_H
+#define JSON_PARSER_H
+
+//TODO Add imports here.
+#include "../Import/Resources/Material/Material_Resource.h"
+#include "../Import/Resources/Object/Object_Resource.h"
+#include "../Components/Camera/Camera_Component.h"
+#include "../Components/Entities/Render_Component.h"
+#include "../Components/Lighting/Light_Component.h"
+#include <string>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief //TODO
+ * @author //TODO
+ */
+class Json_Parser
+{
+
+    public:
+
+        Json_Parser() = delete;
+        ~Json_Parser();
+
+        /**
+         * @brief //TODO
+         * @param pathToJson //TODO
+         */
+        static void parseJSON(const string& pathToJson);
+
+        /**
+         * @brief //TODO
+         * @param exportPath //TODO
+         */
+        static void exportToJSON(const string& exportPath);
+
+        //TODO Implements Getter-Methods for map contents.
+
+    private:
+
+        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Camera_Component> cameras;
+        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Light_Component> lights;
+        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Render_Component> renders;
+        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Object_Resource> objects;
+        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Material_Resource> materials;
+
+};
+
+#endif
