953006845e6cdf6d0342503a148566ebc3c02198
Luke <97346920+lgndluke@users.noreply.github.com>
2024-11-25 14:19:40 +0100

    Development_Components-to-Development
    * Base_Component.h: Base Attribute und Methoden zur Einrichtung und abfangen von UUID
    
    * Base_Component.h: Base Attribute und Methoden zur Einrichtung und abfangen von UUID
    
    * Render_Component.cpp: angepasst, es wurde der falsche Typ vergliechen.
    
    * Base_Component.h: Destructur entfernt
    
    * Base_Component.h + Base_Component.cpp: Konstruktor erstellt und Kommentare in der Header-Datei erstellt.
    
    * Camera_Component.h + Camera_Component.cpp: Kommentare von der Camera_Component.cpp in die Camera_Component.h verschoben und ggf. leicht angepasst.
    
    * Light_Component.h + Light_Component.cpp: Kommentare von der Light_Component.cpp in die Light_Component.h verschoben und ggf. leicht angepasst.
    
    * Render_Component.h + Render_Component.cpp: Kommentare von der Render_Component.cpp in die Render_Component.h verschoben und ggf. leicht angepasst.
    
    * Base_Component.cpp: Leere TODO-Liste entfernt.
    
    * Light_Component.h + Light_Component.cpp: Methode void update(float deltaTime) entfernt.
    
    * Render_Component.h + Render_Component.cpp: Methode void render() const entfernt.
    
    ---------
    
    Co-authored-by: Chrissblau08 <christiankasper12@gmail.com>

--- 4. Software/4.1 Quellen/RaytRazor/Components/Camera/Camera_Component.cpp
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Camera/Camera_Component.cpp
@@ -1,19 +1,33 @@
 #include "Camera_Component.h"
-//TODO Add imports.
 
-//TODO
-// - Define further tasks here.
-// - task2
-// - task3
+Camera_Component::Camera_Component(glm::vec3 position, glm::vec3 front, glm::vec3 up,
+                                   float fov, float near, float far)
+    : position(position), front(front), up(up), fov(fov), nearClip(near), farClip(far)
+{
+}
 
-using namespace std;
+glm::mat4 Camera_Component::getViewMatrix() const
+{
+    return glm::lookAt(position, position + front, up);
+}
 
-// Attributes (Init of global/static Attributes)
+glm::mat4 Camera_Component::getProjectionMatrix(float aspectRatio) const
+{
+    return glm::perspective(glm::radians(fov), aspectRatio, nearClip, farClip);
+}
 
-// Constructor
+void Camera_Component::updateCameraPosition(glm::vec3 newPosition)
+{
+    position = newPosition;
+}
 
-// Destructor
+void Camera_Component::updateCameraOrientation(glm::vec3 newFront, glm::vec3 newUp)
+{
+    front = newFront;
+    up = newUp;
+}
 
-// Private Methods
-
-// Public Methods
+glm::vec3 Camera_Component::getPosition() const
+{
+    return position;
+}
