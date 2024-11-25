c5483fd1c72e9837ee634014278bdda56a5fe9f9
lgndluke <github@lgndluke.com>
2024-11-25 16:11:48 +0100

    Json_Parser.h: Methoden abgeändert und Kommentare eingefügt.

--- 4. Software/4.1 Quellen/RaytRazor/Parsing/Json_Parser.h
+++ 4. Software/4.1 Quellen/RaytRazor/Parsing/Json_Parser.h
@@ -1,21 +1,19 @@
 #ifndef JSON_PARSER_H
 #define JSON_PARSER_H
 
-//TODO Add imports here.
 #include <../../../Utility/External_Libraries/nlohmann/json.hpp>
 #include "../Import/Resources/Material/Material_Resource.h"
 #include "../Import/Resources/Object/Object_Resource.h"
-#include "../Components/Camera/Camera_Component.h"
-#include "../Components/Entities/Render_Component.h"
-#include "../Components/Lighting/Light_Component.h"
+#include "../Components/Base_Component.h"
+#include <boost/uuid/uuid.hpp>
 #include <string>
 #include <map>
 
 using namespace std;
 
 /**
- * @brief //TODO
- * @author //TODO
+ * @brief Diese Klasse bietet Funktionalität, um 3D-Szenenbeschreibungen im JSON Format einzulesen.
+ * @author Christian Kasper, Lukas Jeckle
  */
 class Json_Parser
 {
@@ -26,26 +24,28 @@ class Json_Parser
         ~Json_Parser();
 
         /**
-         * @brief //TODO
-         * @param pathToJson //TODO
+         * @brief     Nimmt die JSON-Datei einer 3D-Szenenbeschreibung entgegen und liest die darin gespeicherten Werte als
+         *            Key-Value Paare, bestehend aus UUID's und Components, ein.
+         *
+         * @param pathToJson          Der Pfad, an dem die zu parsende JSON-Datei zu finden ist. (e.g.: "./JSON/3D_Szene.json"
+         * @return                    Eine Map, die den Inhalt der JSON-Datei als Key-Value Paare aus UUID's und Components enthält.
          */
-        static void parseJSON(const string& pathToJson);
+        static map<boost::uuids::uuid, Base_Component> parseJSON(const string& pathToJson) const;
 
-        /**
-         * @brief //TODO
-         * @param exportPath //TODO
+        /** //TODO Evtl. Make this part of SceneManager?
+         * @brief Exportiert den aktuellen Inhalt der 3D-Szene und speichert diesen an dem übergebenen Export-Pfad.
+         * @param exportPath           Der Pfad, an dem die zu exportierende JSON-Datei gespeicher werden soll.
+         * @return                     True, wenn Export erfolgreich war. Andernfalls, false.
          */
-        static void exportToJSON(const string& exportPath);
+        static boolean exportToJSON(const string& exportPath) const;
 
-        //TODO Implements Getter-Methods for map contents.
+    /*private: //TODO Make this part of the SceneManager.
 
-    private:
-
-        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Camera_Component> cameras;
-        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Light_Component> lights;
-        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Render_Component> renders;
-        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Object_Resource> objects;
-        map<int /*TODO MAKE THIS FROM INT TO UUID*/, Material_Resource> materials;
+        map<boost::uuids::uuid, Camera_Component> cameras;
+        map<boost::uuids::uuid, Light_Component> lights;
+        map<boost::uuids::uuid, Render_Component> renders;
+        map<boost::uuids::uuid, Object_Resource> objects;
+        map<boost::uuids::uuid, Material_Resource> materials;*/
 
 };
 
