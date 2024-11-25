8a507a45669be9b8473033bba92e49fbebef0462
lgndluke <github@lgndluke.com>
2024-11-25 13:41:20 +0100

    Render_Component.h + Render_Component.cpp: Kommentare von der Render_Component.cpp in die Render_Component.h verschoben und ggf. leicht angepasst.

--- 4. Software/4.1 Quellen/RaytRazor/Components/Entities/Render_Component.cpp
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Entities/Render_Component.cpp
@@ -1,68 +1,37 @@
 #include "Render_Component.h"
 
-#include <iostream>
+//TODO:
+// - Implementieren der Render_Component::render() Methode.
+// - cout aufruf in Render_Component::render() durch Logger::log() ersetzten - Logger.h in Header Datei importieren.
 
-/**
- * @brief Konstruktor, der die UUIDs für das Objekt und das Material initialisiert.
- * Falls keine UUID übergeben wird, wird die UUID des Basiskomponenten-Getters verwendet.
- *
- * @param objUUID UUID für das OpenGL-Objekt (Standard: UUID von der Base_Component)
- * @param matUUID UUID für das OpenGL-Material (Standard: UUID von der Base_Component)
- */
 Render_Component::Render_Component(const string& objUUID, const string& matUUID)
 {
-    // Wenn keine UUID übergeben wird, dann verwenden wir die UUID von Base_Component
     this->objUUID = objUUID;
     this->matUUID = matUUID;
 }
 
-/**
- * @brief Gibt die UUID des OpenGL-Objekts zurück.
- *
- * @return string UUID des Objekts
- */
 string Render_Component::getObjUUID() const
 {
     return objUUID;
 }
 
-/**
- * @brief Setzt die UUID für das OpenGL-Objekt.
- *
- * @param newObjUUID Neue UUID für das Objekt
- */
 void Render_Component::setObjUUID(const string& newObjUUID)
 {
     objUUID = newObjUUID;
 }
 
-/**
- * @brief Gibt die UUID des OpenGL-Materials zurück.
- *
- * @return string UUID des Materials
- */
 string Render_Component::getMatUUID() const
 {
     return matUUID;
 }
 
-/**
- * @brief Setzt die UUID für das OpenGL-Material.
- *
- * @param newMatUUID Neue UUID für das Material
- */
 void Render_Component::setMatUUID(const string& newMatUUID)
 {
     matUUID = newMatUUID;
 }
 
-/**
- * @brief Führt das Rendering für das Objekt durch.
- * Diese Methode kann angepasst werden, um die entsprechenden OpenGL-Aufrufe zu tätigen.
- */
 void Render_Component::render() const
 {
-    // Hier könnte Code stehen, der OpenGL-Objekte rendert, basierend auf objUUID und matUUID.
-
+    // Methode implementieren.
     cout << "Rendering object with UUID: " << objUUID << " and material UUID: " << matUUID << endl;
 }
--- 4. Software/4.1 Quellen/RaytRazor/Components/Entities/Render_Component.h
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Entities/Render_Component.h
@@ -2,7 +2,8 @@
 #define RENDER_COMPONENT_H
 
 #include <string>
-#include "../Base_Component.h"  // Annahme: Header für Base_Component
+#include <iostream>
+#include "../Base_Component.h"              // Annahme: Header für Base_Component
 
 using namespace std;
 
@@ -11,27 +12,61 @@ using namespace std;
  * Diese Klasse enthält Referenzen auf OpenGL-Objekte wie Geometrie und Materialien,
  * die für das Rendering eines Objekts notwendig sind.
  *
- * @author Dein Name
+ * @author Christian Kasper
  */
 class Render_Component : public Base_Component
 {
-public:
 
-    Render_Component(const string& objUUID = "", const string& matUUID = "");
+    public:
 
+        /**
+         * @brief Konstruktor, der die UUIDs für das Objekt und das Material initialisiert.
+         * Falls keine UUID übergeben wird, wird die UUID des Basiskomponenten-Getters verwendet.
+         *
+         * @param objUUID          UUID für das OpenGL-Objekt (Standard: UUID von der Base_Component)
+         * @param matUUID          UUID für das OpenGL-Material (Standard: UUID von der Base_Component)
+         */
+        Render_Component(const string& objUUID = "", const string& matUUID = "");
 
-    string getObjUUID() const;
-    void setObjUUID(const string& newObjUUID);
+        /**
+         * @brief Gibt die UUID des OpenGL-Objekts zurück.
+         *
+         * @return string          UUID des Objekts
+         */
+        string getObjUUID() const;
 
-    string getMatUUID() const;
-    void setMatUUID(const string& newMatUUID);
+        /**
+         * @brief Setzt die UUID für das OpenGL-Objekt.
+         *
+         * @param newObjUUID       Neue UUID für das Objekt
+         */
+        void setObjUUID(const string& newObjUUID);
 
+        /**
+         * @brief Gibt die UUID des OpenGL-Materials zurück.
+         *
+         * @return string          UUID des Materials
+         */
+        string getMatUUID() const;
 
-    void render() const;
+        /**
+         * @brief Setzt die UUID für das OpenGL-Material.
+         *
+         * @param newMatUUID       Neue UUID für das Material
+         */
+        void setMatUUID(const string& newMatUUID);
+
+        /**
+         * @brief Führt das Rendering für das Objekt durch.
+         * Diese Methode kann angepasst werden, um die entsprechenden OpenGL-Aufrufe zu tätigen.
+         */
+        void render() const;
 
 private:
-    string objUUID;  // OpenGL-Objekt-ID als UUID
-    string matUUID;  // OpenGL-Material-ID als UUID
+
+    string objUUID;      // OpenGL-Objekt-ID als UUID
+    string matUUID;      // OpenGL-Material-ID als UUID
+
 };
 
-#endif // RENDER_COMPONENT_H
+#endif
