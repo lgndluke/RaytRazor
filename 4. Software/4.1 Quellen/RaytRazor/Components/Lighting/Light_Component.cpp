c6a3ceac552f4e9a3bf868506d8b656d2468b8e9
lgndluke <github@lgndluke.com>
2024-11-25 13:40:59 +0100

    Light_Component.h + Light_Component.cpp: Kommentare von der Light_Component.cpp in die Light_Component.h verschoben und ggf. leicht angepasst.

--- 4. Software/4.1 Quellen/RaytRazor/Components/Lighting/Light_Component.cpp
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Lighting/Light_Component.cpp
@@ -1,63 +1,34 @@
 #include "Light_Component.h"
 
-/**
- * @brief Konstruktor, der die Lichtquelle mit einer Anfangsintensität und -farbe initialisiert.
- *
- * @param intensity Anfangsintensität des Lichts (Standard: 1.0f)
- * @param color Anfangsfarbe des Lichts (Standard: Weiß - glm::vec3(1.0f, 1.0f, 1.0f))
- */
+//TODO:
+// - Implementieren der Light_Component::update(float deltaTime) Methode.
+
 Light_Component::Light_Component(float intensity, glm::vec3 color)
     : intensity(intensity), color(color)
 {
 }
 
-/**
- * @brief Gibt die Intensität des Lichts zurück.
- *
- * @return float Intensität des Lichts
- */
 float Light_Component::getIntensity() const
 {
     return intensity;
 }
 
-/**
- * @brief Setzt die Intensität des Lichts.
- *
- * @param newIntensity Neue Intensität des Lichts
- */
 void Light_Component::setIntensity(float newIntensity)
 {
     intensity = newIntensity;
 }
 
-/**
- * @brief Gibt die Farbe des Lichts zurück.
- *
- * @return glm::vec3 Farbe des Lichts (RGB)
- */
 glm::vec3 Light_Component::getColor() const
 {
     return color;
 }
 
-/**
- * @brief Setzt die Farbe des Lichts.
- *
- * @param newColor Neue Farbe des Lichts (RGB)
- */
 void Light_Component::setColor(const glm::vec3& newColor)
 {
     color = newColor;
 }
 
-/**
- * @brief Aktualisiert die Eigenschaften des Lichts basierend auf einer Zeitdifferenz.
- * Dies könnte für dynamische Lichteffekte oder Animationen nützlich sein.
- *
- * @param deltaTime Zeitdifferenz, die seit dem letzten Update vergangen ist
- */
 void Light_Component::update(float deltaTime)
 {
-
+    // Methode implementieren.
 }
--- 4. Software/4.1 Quellen/RaytRazor/Components/Lighting/Light_Component.h
+++ 4. Software/4.1 Quellen/RaytRazor/Components/Lighting/Light_Component.h
@@ -1,34 +1,69 @@
 #ifndef LIGHT_COMPONENT_H
 #define LIGHT_COMPONENT_H
 
-#include <glm/glm.hpp>  // Für glm::vec3
-#include "../Base_Component.h"  // Annahme: Header für Base_Component
+#include <glm/glm.hpp>                      // Für glm::vec3
+#include "../Base_Component.h"              // Annahme: Header für Base_Component
 
 /**
  * @brief Repräsentiert eine Lichtquelle in der Szene mit Eigenschaften wie Intensität und Farbe.
  * Diese Klasse kann für Lichteffekte verwendet werden, die auf ein Objekt in einer 3D-Szene angewendet werden.
  *
- * @author Dein Name
+ * @author Christian Kasper
  */
 class Light_Component : public Base_Component
 {
-public:
-    Light_Component(float intensity = 1.0f, glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f));
 
-    float getIntensity() const;
+    public:
 
-    void setIntensity(float newIntensity);
+        /**
+         * @brief Konstruktor, der die Lichtquelle mit einer Anfangsintensität und -farbe initialisiert.
+         *
+         * @param intensity        Anfangsintensität des Lichts (Standard: 1.0f)
+         * @param color            Anfangsfarbe des Lichts (Standard: Weiß - glm::vec3(1.0f, 1.0f, 1.0f))
+         */
+        Light_Component(float intensity = 1.0f, glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f));
 
-    glm::vec3 getColor() const;
+        /**
+         * @brief Gibt die Intensität des Lichts zurück.
+         *
+         * @return float           Intensität des Lichts
+         */
+        float getIntensity() const;
 
-    void setColor(const glm::vec3& newColor);
+        /**
+         * @brief Setzt die Intensität des Lichts.
+         *
+         * @param newIntensity     Neue Intensität des Lichts
+         */
+        void setIntensity(float newIntensity);
 
-    void update(float deltaTime);
+        /**
+         * @brief Gibt die Farbe des Lichts zurück.
+         *
+         * @return glm::vec3       Farbe des Lichts (RGB)
+         */
+        glm::vec3 getColor() const;
 
-private:
-    float intensity;
+        /**
+         * @brief Setzt die Farbe des Lichts.
+         *
+         * @param newColor         Neue Farbe des Lichts (RGB)
+         */
+        void setColor(const glm::vec3& newColor);
 
-    glm::vec3 color;
+        /**
+         * @brief Aktualisiert die Eigenschaften des Lichts basierend auf einer Zeitdifferenz.
+         * Könnte für dynamische Lichteffekte oder Animationen nützlich sein.
+         *
+         * @param deltaTime        Zeitdifferenz, die seit dem letzten Update vergangen ist
+         */
+        void update(float deltaTime);
+
+    private:
+
+        float intensity;
+        glm::vec3 color;
+
 };
 
-#endif  // LIGHT_COMPONENT_H
+#endif
