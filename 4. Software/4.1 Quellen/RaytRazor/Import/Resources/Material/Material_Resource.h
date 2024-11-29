787861edaf61422177f89fa661f6496d98f8bce9
lgndluke <github@lgndluke.com>
2024-11-29 13:07:09 +0100

    Material_Resource.h: "public" keyword zur Vererbung hinzugefügt und fehlendes Komma ergänzt.

--- 4. Software/4.1 Quellen/RaytRazor/Import/Resources/Material/Material_Resource.h
+++ 4. Software/4.1 Quellen/RaytRazor/Import/Resources/Material/Material_Resource.h
@@ -32,7 +32,7 @@ struct Material
  * @brief Abgeleitete Klasse Material_Resource repräsentiert eine Material Ressource.
  * @author Lukas Jeckle
  */
-class Material_Resource : Base_Resource
+class Material_Resource : public Base_Resource
 {
 
     public:
@@ -46,7 +46,7 @@ class Material_Resource : Base_Resource
          * @param indices                Indices der Material_Resource.
          * @param materials              Materials der Material_Resource.
          */
-        Material_Resource(const boost::uuids::uuid uuid, const string& path
+        Material_Resource(const boost::uuids::uuid uuid, const string& path,
                           const std::vector<int>& indices, const std::vector<Material>& materials);
 
         /**
