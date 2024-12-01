b190ea9e0466ac29d01a24596a62ce7b5a29d298
lgndluke <github@lgndluke.com>
2024-12-01 13:01:18 +0100

    Main_Scene.h & Main_Scene.cpp: Erste sehr grobe Struktur implementiert.

--- 4. Software/4.1 Quellen/RaytRazor/UI/Scenes/Main/Main_Scene.h
+++ 4. Software/4.1 Quellen/RaytRazor/UI/Scenes/Main/Main_Scene.h
@@ -1,24 +1,41 @@
 #ifndef MAIN_SCENE_H
 #define MAIN_SCENE_H
 
-//TODO Add imports here.
+#include <nanogui/nanogui.h>
 
 using namespace std;
+using namespace nanogui;
 
 /**
- * @brief //TODO
- * @author //TODO
+ * @brief Die Klasse Main_Scene repräsentiert das Haupt-Fenster der Anwendung.
+ * @author Lukas Jeckle
  */
-class Main_Scene
+class Main_Scene final : public Screen
 {
 
     public:
 
-        //TODO add public Method definitions.
+        /**
+         * @brief Konstruktor zum Erstellen eines Main_Scene Objektes.
+         *
+         * @param window_width         Die Breite des zu erstellenden Fensters.
+         * @param window_height        Die Höhe des zu erstellenden Fensters.
+         * @param window_title         Der Titel des zu erstellenden Fensters.
+         * @param is_resizeable        Statische Fenstergröße (false), Dynamische Fenstergröße (true)
+         */
+        Main_Scene(const int window_width,
+                   const int window_height,
+                   const string& window_title,
+                   const bool is_resizeable);
+
+        /**
+         * @brief Methode, um die Main_Scene zu aktualisieren.
+         */
+        void update();
 
     private:
 
-        //TODO add private Method definitions.
+        //
 
 };
 
