9083476d47855cb214e2c30374457ff8f9893432
lgndluke <github@lgndluke.com>
2024-12-01 11:30:48 +0100

    Scene_Manager.h & Scene_Manager.cpp: Erste unvollständige Version der neuen Struktur erstellt.

--- 4. Software/4.1 Quellen/RaytRazor/UI/SceneManager/Scene_Manager.cpp
+++ 4. Software/4.1 Quellen/RaytRazor/UI/SceneManager/Scene_Manager.cpp
@@ -1,28 +1,22 @@
 #include "Scene_Manager.h"
-//TODO Add imports.
 
-#include "nanogui/nanogui.h"
-//TODO
-// - Define further tasks here.
-// - task2
-// - task3
+//TODO's:
+// -> Scene_Manager::update_canvas() implementieren.
+// -> Scene_Manager::update_component() implementieren.
 
-using namespace std;
+void Scene_Manager::update_canvas(Main_Scene main_Scene)
+{
+    // Think about structure.
+}
 
+void Scene_Manager::update_component(const int id)
+{
 
-using namespace nanogui;
+    // 1. Get Component that is affected.
+    Base_Component component = this->components[id];
 
+    // 2. Find out what kind of component it is.
 
+        // 3. Change values depending on component type.
 
-
-
-
-// Attributes (Init of global/static Attributes)
-
-// Constructor
-
-// Destructor
-
-// Private Methods
-
-// Public Methods
+}
--- 4. Software/4.1 Quellen/RaytRazor/UI/SceneManager/Scene_Manager.h
+++ 4. Software/4.1 Quellen/RaytRazor/UI/SceneManager/Scene_Manager.h
@@ -1,24 +1,51 @@
 #ifndef SCENE_MANAGER_H
 #define SCENE_MANAGER_H
 
-//TODO Add imports here.
+#include "../../Components/Base_Component.h"
+#include "../../Import/Resources/Base_Resource.h"
+#include "../Scenes/Main/Main_Scene.h"
+#include <nanogui/nanogui.h>
 
 using namespace std;
+using namespace nanogui;
 
 /**
- * @brief //TODO
- * @author //TODO
+ * @brief Die Klasse Scene_Manager verwaltet die Ressourcen der Szenen.
+ * @author Lukas Jeckle
  */
 class Scene_Manager
 {
 
     public:
 
-        //TODO add public Method definitions.
+        /**
+         * @brief Statische Klasse ohne Konstruktor.
+         */
+        Scene_Manager() = delete;
+
+        /**
+         * @brief Methode, um den Inhalt der @link Main_Scene zu aktualisieren.
+         * @param main_Scene                Die zu aktualisierende Main_Scene.
+         */
+        void update_canvas(Main_Scene main_Scene);
+
+        /**
+         * @brief Methode, um die Werte eines @link Base_Component Objekts zu aktualisieren.
+         * @param id                        Die ID des zu aktualisierenden Base_Components.
+         */
+        void update_component(const int id);
+
+        /**
+         *
+         *
+         */
+        //void update_resource();
 
     private:
 
-        //TODO add private Method definitions.
+        vector<int> ids;
+        map<int, Base_Component> components;
+        map<int, Base_Resource> resources;
 
 };
 
