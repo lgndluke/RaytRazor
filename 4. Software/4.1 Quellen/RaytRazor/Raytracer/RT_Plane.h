d00354030c57a573aa9dceba876e75ec84782127
leon <leon@thu.com>
2024-12-02 16:55:22 +0100

    RT_Plane: .h und .cpp für Ebene hinzugefügt

---
+++ 4. Software/4.1 Quellen/RaytRazor/Raytracer/RT_Plane.h
@@ -0,0 +1,17 @@
+#ifndef PLANE_H
+#define PLANE_H
+
+#include"RT_Object.h"
+#include"rt_geometricTransform.h"
+
+// TODO: Kommentare schreiben
+
+class Plane : public Object {
+    public:
+        Plane();
+        virtual ~Plane() override;
+        virtual bool hit(const Ray &ray, Vector<double> &point, Vector<double> &normal, Vector<double> &color) override;
+    private:
+};
+
+#endif //PLANE_H
