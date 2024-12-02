d0fd5285a62b1389e8f7bab3be43b325bff2a0d2
leon <leon@thu.com>
2024-12-02 16:56:13 +0100

    RT_Image: .h und .cpp für das Bild hinzugefügt

--- 4. Software/4.1 Quellen/RaytRazor/Raytracer/RT_Image.h
+++ 4. Software/4.1 Quellen/RaytRazor/Raytracer/RT_Image.h
@@ -9,22 +9,27 @@
 #include <vector>
 #include <SDL.h>
 
-class RT_Image {
-    public:
-        RT_Image();
-        ~RT_Image();
-        void initialize(const int width, const int height, SDL_Renderer* renderer);
-        void setPx(const int x, const int y, const double red, const double green, const double blue);
-        void display();
-    private:
-        Uint32 convertColor(const double red, const double green, const double blue);
-        void initTexture();
-        std::vector<std::vector<double>> img_r_channel;
-        std::vector<std::vector<double>> img_g_channel;
-        std::vector<std::vector<double>> img_b_channel;
-        int img_width, img_height;
-        SDL_Renderer* img_renderer;
-        SDL_Texture* img_texture;
+// TODO: Kommentare schreiben
+class Image {
+public:
+    Image();
+    ~Image();
+    void Initialize(const int xSize, const int ySize, SDL_Renderer *pRenderer);
+    void SetPixel(const int x, const int y, const double red, const double green, const double blue);
+    int getXSize();
+    int getYSize();
+    void Display();
+private:
+    Uint32 ConvertColor(const double red, const double green, const double blue);
+    void InitTexture();
+    void computeMaxValues();
+    std::vector<std::vector<double>> m_rChannel;
+    std::vector<std::vector<double>> m_gChannel;
+    std::vector<std::vector<double>> m_bChannel;
+    int m_xSize, m_ySize;
+    double m_maxRed, m_maxGreen, m_maxBlue, m_overallMax;
+    SDL_Renderer *m_pRenderer;
+    SDL_Texture *m_pTexture;
 };
 
 #endif //RT_IMAGE_H
