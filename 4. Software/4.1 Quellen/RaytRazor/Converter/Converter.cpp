1dc4281d4192d41d42ead65de8bac3554dc66e81
Dennis Welsch <d.welsch3459@gmail.com>
2024-12-04 17:55:13 +0100

    Converter.cpp / .h :: theoretisch funktionsfähige convertierung eingefügt

--- 4. Software/4.1 Quellen/RaytRazor/Converter/Converter.cpp
+++ 4. Software/4.1 Quellen/RaytRazor/Converter/Converter.cpp
@@ -1,1 +1,30 @@
 #include "Converter.h"
+
+nanogui::MatrixXu Convert_to_Indices(const Object_Resource& source){
+
+    vector<int> input = source.get_indices();
+    nanogui::MatrixXu output(3,input.size()/3);
+
+    for (int i = 0; i < input.size()/3; i++) {
+        output.col(i) << input[i], input[i+1], input[i+2];
+    }
+}
+
+nanogui::MatrixXf Convert_to_Vertices(const Object_Resource& source){
+
+    vector<Vertex> input = source.get_vertices();
+    nanogui::MatrixXf output(3,input.size()/3);
+
+    for (int i = 0; i < input.size(); ++i) {
+        output.col(i) << input[i].position.x, input[i].position.y, input[i].position.z;
+    }
+}
+
+nanogui::MatrixXf Convert_to_Color(const Object_Resource& source){
+    vector<Vertex> input = source.get_vertices();
+    nanogui::MatrixXf output(3,input.size()/3);
+
+    for (int i = 0; i < input.size(); ++i) {
+        output.col(i) << input[i].color.x, input[i].color.y, input[i].color.z;
+    }
+}
--- 4. Software/4.1 Quellen/RaytRazor/Converter/Converter.h
+++ 4. Software/4.1 Quellen/RaytRazor/Converter/Converter.h
@@ -2,6 +2,10 @@
 #define CONVERTER_H
 
 
+#include "../Import/Resources/Object/Object_Resource.h"
+#include <nanogui/nanogui.h>
+#include <vector>
+
 /**
  * @brief //TODO
  * @author
@@ -9,6 +13,15 @@
 class Converter {
 
     public:
+
+
+    Converter() = delete;
+
+    nanogui::MatrixXu Convert_to_MatIndices(const Object_Resource& source){};
+
+    nanogui::MatrixXf Convert_to_MatVertices(const Object_Resource& source){};
+
+    nanogui::MatrixXf Convert_to_MatColor(const Object_Resource& source){};
 
     private:
 
